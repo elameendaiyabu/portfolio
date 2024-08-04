dd7c809f1ac6720f2e400453445bf9a00d531991
elameendaiyabu <elameendaiyabu@gmail.com>
2024-08-04 14:15:41 +0000

    added hover effect

--- src/components/contents.tsx
+++ src/components/contents.tsx
@@ -1,44 +1,45 @@
+import Link from "next/link";
 import AboutSection from "./about";
 import ExperienceSection from "./experience";
 
 export const content = [
   {
-    title: "ABOUT",
+    title: <div id="about">ABOUT</div>,
     description: <AboutSection />,
     content: (
       <div className="h-full w-full bg-[] flex flex-col gap-4 text-white">
         ABOUT
         <span className="text-muted-foreground flex flex-col gap-4 ">
-          <div>EXPERIENCE</div>
-          <div>PROJECTS</div>
+          <Link href="/#experience">EXPERIENCE</Link>
+          <Link href="/#projects">PROJECTS</Link>
         </span>
       </div>
     ),
   },
   {
-    title: "EXPERIENCE",
+    title: <div id="experience">EXPERIENCE</div>,
     description: <ExperienceSection />,
     content: (
       <div className="h-full w-full flex flex-col gap-4 bg-[] text-white">
         <span className="text-muted-foreground">
-          <div>ABOUT</div>
+          <Link href="/#about">ABOUT</Link>
         </span>
         EXPERIENCE
         <span className="text-muted-foreground flex flex-col gap-4 ">
-          <div>PROJECTS</div>
+          <Link href="/#projects">PROJECTS</Link>
         </span>
       </div>
     ),
   },
   {
-    title: "PROJECTS",
+    title: <div id="projects">PROJECTS</div>,
     description:
       "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
     content: (
       <div className="h-full flex flex-col gap-4  w-full bg-[] text-white">
         <span className="text-muted-foreground flex flex-col gap-4 ">
-          <div>ABOUT</div>
-          <div>EXPERIENCE</div>
+          <Link href="/#about">ABOUT</Link>
+          <Link href="/#experience">EXPERIENCE</Link>
         </span>
         PROJECTS
       </div>
--- src/components/homePage.tsx
+++ src/components/homePage.tsx
@@ -10,6 +10,7 @@ import {
   faLinkedin,
   faXTwitter,
 } from "@fortawesome/free-brands-svg-icons";
+import { CardSpotlight } from "./ui/card-spotlight";
 
 export const HomePage = ({
   content,
@@ -49,106 +50,110 @@ export const HomePage = ({
 
   return (
     <motion.div
-      className="h-screen lg:p-10 py-12 overflow-y-auto flex flex-col lg:flex-row-reverse lg:justify-center bg-slate-900 relative space-x-10 rounded-md "
+      className="h-screen overflow-y-auto  bg-slate-900 space-x-10 rounded-md "
       ref={ref}
     >
-      <div className="lg:hidden flex flex-col gap-4 px-10">
-        <h2 className="scroll-m-20 text-2xl font-extrabold tracking-tight ">
-          El-ameen Daiyabu
-        </h2>
-        <h3 className="scroll-m-20 text-lg tracking-tight transition-colors first:mt-0">
-          Full-Stack Web Developer
-        </h3>
-        <p className="text-muted-foreground ">
-          I build the things i want and the things you want.
-        </p>
-        <div className="flex gap-6">
-          <FontAwesomeIcon
-            icon={faXTwitter}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
-          <FontAwesomeIcon
-            icon={faInstagram}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
-        </div>
-      </div>
-      <div className="div relative flex items-start lg:ml-56 lg:w-1/2 lg:px-4">
-        <div className=" pr-10 lg:pr-0 max-w-2xl">
-          {content.map((item, index) => (
-            <div key={item.title + index} className="my-20">
-              <motion.h2
-                initial={{
-                  opacity: 0,
-                }}
-                animate={{
-                  opacity: activeCard === index ? 1 : 0.3,
-                }}
-                className="lg:text-2xl text-lg font-bold text-slate-100"
-              >
-                {item.title}
-              </motion.h2>
-              <motion.div
-                initial={{
-                  opacity: 0,
-                }}
-                animate={{
-                  opacity: activeCard === index ? 1 : 0.3,
-                }}
-                className="lg:text-lg text-muted-foreground lg:mb-36 mt-4 lg:mt-10"
-              >
-                {item.description}
-              </motion.div>
-            </div>
-          ))}
-          <div className="mb-44" />
-        </div>
-      </div>
-      <div
-        className={cn(
-          "hidden lg:flex flex-col gap-10 items-center justify-between h-[30rem] w-[30rem] rounded-md sticky top-10 ",
-          contentClassName,
-        )}
-      >
-        <div className=" w-full">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <CardSpotlight className="flex relative bg-slate-900 flex-col lg:flex-row-reverse lg:justify-between">
+        <div className="lg:hidden flex flex-col gap-4 ">
+          <h2 className="scroll-m-20 text-2xl font-extrabold tracking-tight ">
             El-ameen Daiyabu
-          </h1>
-          <h3 className="mt-4 scroll-m-20 pb-2 text-xl font-semibold tracking-tight transition-colors first:mt-0">
+          </h2>
+          <h3 className="scroll-m-20 text-lg tracking-tight transition-colors first:mt-0">
             Full-Stack Web Developer
           </h3>
-          <p className="mt-2 text-muted-foreground scroll-m-20 tracking-tight">
+          <p className="text-muted-foreground ">
             I build the things i want and the things you want.
           </p>
+          <div className="flex gap-6">
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+          </div>
         </div>
-        <div className="w-full h-28">{content[activeCard].content ?? null}</div>
-        <div className="w-full flex gap-5">
-          <FontAwesomeIcon
-            icon={faXTwitter}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
-          <FontAwesomeIcon
-            icon={faGithub}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
-          <FontAwesomeIcon
-            icon={faInstagram}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
-          />
+        <div className="div relative flex items-start lg:ml-56 lg:w-1/2 lg:px-4">
+          <div className=" lg:pr-0 max-w-2xl">
+            {content.map((item, index) => (
+              <div key={item.title + index} className="my-20">
+                <motion.h2
+                  initial={{
+                    opacity: 0,
+                  }}
+                  animate={{
+                    opacity: activeCard === index ? 1 : 0.3,
+                  }}
+                  className="lg:text-2xl text-lg font-bold text-slate-100"
+                >
+                  {item.title}
+                </motion.h2>
+                <motion.div
+                  initial={{
+                    opacity: 0,
+                  }}
+                  animate={{
+                    opacity: activeCard === index ? 1 : 0.3,
+                  }}
+                  className="lg:text-lg text-muted-foreground lg:mb-36 mt-4 lg:mt-10"
+                >
+                  {item.description}
+                </motion.div>
+              </div>
+            ))}
+            <div className="mb-44" />
+          </div>
         </div>
-      </div>
+        <div
+          className={cn(
+            "hidden lg:flex flex-col gap-10 items-center justify-between h-[30rem] w-[30rem] rounded-md sticky top-10 ",
+            contentClassName,
+          )}
+        >
+          <div className=" w-full">
+            <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+              El-ameen Daiyabu
+            </h1>
+            <h3 className="mt-4 scroll-m-20 pb-2 text-xl font-semibold tracking-tight transition-colors first:mt-0">
+              Full-Stack Web Developer
+            </h3>
+            <p className="mt-2 text-muted-foreground scroll-m-20 tracking-tight">
+              I build the things i want and the things you want.
+            </p>
+          </div>
+          <div className="w-full h-28">
+            {content[activeCard].content ?? null}
+          </div>
+          <div className="w-full flex gap-5">
+            <FontAwesomeIcon
+              icon={faXTwitter}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="w-7 h-7 text-muted-foreground hover:cursor-pointer hover:text-white"
+            />
+          </div>
+        </div>
+      </CardSpotlight>
     </motion.div>
   );
 };
